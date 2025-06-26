d6731e8d1f2e0a449b69898136bd2e7e7d757cdd
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-06-26 14:52:03 +0200

    CH3/L7: add game to client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strings"
+
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/qol"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
-	"log"
 )
 
 func main() {
 
 	fmt.Println("Starting Peril client...")
 	conn := pubsub.GetConnection()
+	//nolint
 	defer conn.Close()
 
 	username, err := gamelogic.ClientWelcome()
@@ -25,9 +28,48 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to declare and bind queue: %v", err)
 	}
+
+	gameState := gamelogic.NewGameState(username)
+	playGame(gameState)
+
+	//nolint
 	defer chConn.Close()
 
 	qol.WaitForSignalToKill()
 
 	fmt.Println("Program aborted! Connection closed.")
+}
+
+func playGame(gameState *gamelogic.GameState) {
+	for isOver := false; !isOver; {
+		input := gamelogic.GetInput()
+		if len(input) == 0 {
+			continue
+		}
+		switch strings.TrimSpace(strings.ToLower(input[0])) {
+		//spawn <location> <rank>
+		case "spawn":
+			err := gameState.CommandSpawn(input)
+			if err != nil {
+				fmt.Println(err)
+			}
+		//move <destination> <unit-id>
+		case "move":
+			_, err := gameState.CommandMove(input)
+			if err != nil {
+				fmt.Println(err)
+			}
+		case "status":
+			gameState.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			fmt.Println("Spamming not allowed yet!")
+		case "quit":
+			gamelogic.PrintQuit()
+			isOver = true
+		default:
+			fmt.Println("I can't understand Your command! Try using `help` :)")
+		}
+	}
 }
